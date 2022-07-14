9aada0f090682b89dbdb7aff4b79c61a23e244b2
ShuvenduSamanta <shuvendu247@gmail.com>
2022-07-14 16:09:57 +0530

    first commit

---
+++ src/main/java/com/shuvendusamanta/paytoearn/PayToEarnApplication.java
@@ -0,0 +1,13 @@
+package com.shuvendusamanta.paytoearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayToEarnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayToEarnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shuvendusamanta/paytoearn/PayToEarnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuvendusamanta.paytoearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayToEarnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
